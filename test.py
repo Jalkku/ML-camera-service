1d222898779083cb5ab71ee9b264208c3327d3e9
Jalkku <jalihautala@hotmail.com>
2017-01-02 15:34:11 +0000

    Bbox integration

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import numpy as np
+import subprocess
 from datetime import datetime
 
 class ImageProcessing:
@@ -13,29 +14,9 @@ class ImageProcessing:
 		difference = 255-cv2.absdiff(a, b)
 		return difference
 		
-		
-class BoundingBox:
-	def __init__(self, img, x, y):
-		width, height = img.shape
-		print(width,height)
-		self.covered = np.zeros((height, width,1), np.uint8)
-		self.boundXmin = 0
-		self.boundXmax = height
-		self.boundYmin = 0
-		self.boundYmax = width
-		while len(neighbors) != 0:
-			current = neighbors[0]
-			neighbors.pop(0)
-			self.covered[current[0], current[1]] == 255
-			for x_ in range(current[0]-1, current[0]+1):
-				for y_ in range(current[1]-1, current[1]+1):
-					print("trying ", x_, y_)
-					if img[x_, y_] == 255 and (x_, y_) not in neighbors and self.covered[x_, y_] == 0:
-						print("appent ", x_, y_)
-						neighbors.append((x_, y_))
-			print(neighbors)
-						
-		print(neighbors)
+	def blur(self, img):
+		blurred = cv2.medianBlur(img, 7)
+		return blurred
 					
 class Capture:
 	def __init__(self, camera, width, height):
@@ -43,8 +24,11 @@ class Capture:
 		self.fgbg = cv2.BackgroundSubtractorMOG()
 		self.camera.set(3, width)
 		self.camera.set(4, height)
+		subprocess.call("camera_settings.sh", shell=True)
+		test_img = self.get_image()
+		cv2.imwrite("mnt/cifs/test.jpg", test_img)
 		self.mainloop()
-	 
+
 	def get_image(self):
 		retval, im = self.camera.read()
 		gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -52,23 +36,39 @@ class Capture:
 
 	def mainloop(self):
 		previous_image = self.get_image()
+		bg = self.get_image()
 		while True:
 			try:
-				'''
-				for i in xrange(10):
-					temp = get_image()'''
-				tic = time.time()
+				#tic = time.time()
 				camera_capture = self.get_image()
 				mse_ = processing.mse(camera_capture, previous_image)
-				if mse_ > 50:
-					print("Movement with ", mse_, " MSE")
-					file = "/mnt/cifs/"+datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')+".jpg"
-					cv2.imwrite(file, camera_capture)
-					print("Captured ", file)
+				#print(mse_)
+				original_image = camera_capture.copy()
+				if mse_ > 150:
+					print("Captured with ", mse_, " MSE")
+					camera_capture_ = processing.blur(camera_capture)
+					temp_bg = processing.blur(bg)
+					difference = processing.diff(camera_capture_, temp_bg)
+					_, mask = cv2.threshold(difference,240,255,cv2.THRESH_BINARY_INV)
+					#result = 255 - original_image*mask
+					# resize mask for faster operations
+					#mask = cv2.resize(mask,None,fx=0.3, fy=0.3, interpolation = cv2.INTER_CUBIC)
+					#h, w = mask.shape'
 
-				previous_image = camera_capture
-				toc = time.time()
-				print("FPS: "+ str(1/(toc-tic)))
+					contours, _ = cv2.findContours(mask,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+					marked_image = original_image.copy()
+					for c in contours:
+						x,y,w,h = cv2.boundingRect(c)
+						if w > 50 and h > 50:
+							cv2.rectangle(marked_image,(x,y),(x+w,y+h),(255,0,0),2)
+					filename = "/mnt/cifs/"+datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')+".jpg"
+					filename_m = "/mnt/cifs/"+datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')+"_m.jpg"
+					cv2.imwrite(filename, marked_image)
+					#cv2.imwrite(filename_m, mask)
+				else:
+					bg = previous_image.copy()
+				previous_image = original_image.copy()
+				#toc = time.time()
 
 			except KeyboardInterrupt: 
 				print("Stopping...")
@@ -76,8 +76,9 @@ class Capture:
 				break
 
 processing = ImageProcessing()
-
+capture = Capture(cv2.VideoCapture(0), 1200, 1080)
 # for testing
+'''
 previous_image = cv2.imread('test_images/blank.jpg')
 previous_image = cv2.cvtColor(previous_image, cv2.COLOR_BGR2GRAY)
 previous_image = cv2.bilateralFilter(previous_image,9,75,75)
@@ -101,11 +102,11 @@ if mse_ > 50:
 	print(x,y,w,h)
 	cv2.rectangle(original_image,(x,y),(x+w,y+h),(128,255,128),2)
 	#bbox
-	'''
 	for x in range(h-1):
 		for y in range(w-1):
 			if mask[x, y] == 1:
 				bbox = BoundingBox(mask, x, y)
-				break;'''
+				break;
 	cv2.imwrite("/mnt/cifs/result.jpg", original_image)
 #capture = Capture(cv2.VideoCapture(0), 1000, 1000)
+'''
